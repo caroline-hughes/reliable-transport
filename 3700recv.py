b637be203f3955361a8a5af9fea22a33e464bb46
Caroline Hughes <carolinehughes1717@gmail.com>
2022-10-30 20:32:06 -0400

    sender window size 4, don't need to verify order or recieved acks. passing level 3

--- 3700recv.py
+++ 3700recv.py
@@ -3,6 +3,11 @@
 import argparse, socket, time, json, select, struct, sys, math
 
 class Receiver:
+    # dict mapping seqnum -> msg, for packets recieved out of order
+    stored_data = {}
+    expected_seqnum = 1
+    seen = []
+
     def __init__(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.bind(('0.0.0.0', 0))
@@ -19,9 +24,22 @@ class Receiver:
         sys.stderr.write(message + "\n")
         sys.stderr.flush()
 
+    def send_ack(self, msg, seqnum):
+        self.log('sending ack for seqn %s' % seqnum)
+        print(msg["data"], end='', flush=True) # print
+        self.send({ "type": "ack", "seqnum": seqnum }) # send the ack
+        self.seen.append(seqnum) # append it to list of seen seq nums
+        self.expected_seqnum += 1 # increment
+
+    def check_next_ack(self):
+        next_ack = self.stored_data.get(self.expected_seqnum)
+        if next_ack is not None:
+            self.send_ack(next_ack, self.expected_seqnum)
+            self.check_next_ack() # call recursive check for next acks 
+        else:
+            return
+
     def run(self):
-        expected_seqnum = 1
-        seen = []
         while True:
             socks = select.select([self.socket], [], [])[0]
             for conn in socks:
@@ -38,24 +56,23 @@ class Receiver:
                 seqnum = msg["seqnum"] # get packet seq number
 
                 # if next seq number
-                if seqnum == expected_seqnum:
-                    # Print out the data to stdout
-                    print(msg["data"], end='', flush=True)
-                    self.send({ "type": "ack", "seqnum": seqnum })
-                    
-                    seen.append(seqnum)
-                    expected_seqnum += 1
+                self.log('if seqnum == self.expected_seqnum: '+ str(seqnum) + '==' + str(self.expected_seqnum) + '?')
+                if seqnum == self.expected_seqnum:
+                    self.send_ack(msg, seqnum)
+                    self.check_next_ack()
 
                 # if duplicate seq number 
-                elif seqnum in seen:
-                    strtoprint = 'got duplicate packet ' + str(seqnum) + "\n"
-                    self.log(strtoprint)
+                elif seqnum in self.seen:
+                    self.log("got duplicate packet %s \n" % str(seqnum))
 
                 # if out of order
                 else:
-                    self.log('nothing')
-                    # TODO
+                    self.log("got out of order packet %s \n" % str(seqnum))
+                    # save the datagram to the dict, don't ack yet
+                    self.stored_data[seqnum] = msg
+                    
         return
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='receive data')
--- 3700send.py
+++ 3700send.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env -S python3 -u
 
 import argparse, socket, time, json, select, struct, sys, math
+from re import L
 
 DATA_SIZE = 1375
+WINDOW_SIZE = 4
 
 class Sender:
+
     def __init__(self, host, port):
         self.host = host
         self.remote_port = int(port)
@@ -20,9 +23,18 @@ class Sender:
     def send(self, message):
         self.socket.sendto(json.dumps(message).encode('utf-8'), (self.host, self.remote_port))
 
+    def got_all_acks(self, acks, seqnum):
+        for i in range(seqnum):
+            if acks[i] != 1:
+                return False
+        return True
+
+
     def run(self):
         seqnum = 1
         expected_ack_seqnum = 1
+
+        # acks = [0,0,0,0,0,0,0,0] # like, [1, 1, 1, 1, 1, 1, 1, 1] for test 3-1. should be 8 ones
 
         curr_acks = 0
         while True:
@@ -34,47 +46,40 @@ class Sender:
                 if conn == self.socket:
                     k, addr = conn.recvfrom(65535) # get an ack
                     msg = json.loads(k.decode('utf-8'))
-                    acked_seqnum = msg["seqnum"]
-                    if expected_ack_seqnum == acked_seqnum:
-                        self.log("GOT EXPECTED ACK: '%s'" % str(acked_seqnum))
-                        expected_ack_seqnum += 1
+
+                    # acked_seqnum = msg["seqnum"]
+                    # in case the ack comes back out of order, mark it in array
+                    # acks[acked_seqnum - 1] = 1
+                    # self.log("acks arr: '%s'" % acks)
+
+                    # dont actually need this functionality. (yet(?))
+                    # if expected_ack_seqnum == acked_seqnum:
+                    #     self.log("got next ACK: '%s'" % str(acked_seqnum))
+                    #     expected_ack_seqnum += 1
+                    # else:
+                    #     self.log("got out of order ACK: '%s'" % str(acked_seqnum))
                         
-                        # if second ack
-                        if curr_acks == 1:
-                            self.waiting = False # done waiting
-                            curr_acks = 0
-                        else:
-                            curr_acks += 1
+                    if curr_acks == WINDOW_SIZE - 1:
+                        self.waiting = False # done waiting
+                        curr_acks = 0
                     else:
-                        self.log("GOT UNEXP ACK: '%s'" % str(acked_seqnum))
+                        curr_acks += 1
+
 
                     self.log("Received message '%s'" % msg)
                     
                 elif conn == sys.stdin: # if got an ack, read more data from stdin
+                    for n in range(WINDOW_SIZE):
+                        data = sys.stdin.read(DATA_SIZE)
+                        
+                        if len(data) == 0:
+                            self.log("All done!")
+                            sys.exit(0)
 
-                    # send first
-                    data1 = sys.stdin.read(DATA_SIZE)
-                    
-                    if len(data1) == 0:
-                        self.log("All done!")
-                        sys.exit(0)
-
-                    
-                    msg1 = { "type": "msg", "seqnum": seqnum, "data": data1 }
-                    self.log("Sending message '%s'" % msg1)
-                    self.send(msg1)
-                    seqnum+=1
-
-                    # send second
-                    data2 = sys.stdin.read(DATA_SIZE)
-                    if len(data2) == 0:
-                        self.log("All done!")
-                        sys.exit(0)
-
-                    msg2 = { "type": "msg", "seqnum": seqnum, "data": data2 }
-                    self.log("Sending message '%s'" % msg2)
-                    self.send(msg2)
-                    seqnum+=1
+                        msg = { "type": "msg", "seqnum": seqnum, "data": data }
+                        self.log("Sending message '%s'" % msg)
+                        self.send(msg)
+                        seqnum += 1
 
                     self.waiting = True
 
